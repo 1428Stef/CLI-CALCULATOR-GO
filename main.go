43fe9b23c617b781cbc2ffc20ce8b372e9a1161b
1428 <romafon678@gmail.com>
2025-09-16 20:59:38 +0300

    Update main.go
    -remove head 
    -Corrected typo "fist" to "first"

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"fmt"
-<<<<<<< HEAD
-
 	"github.com/fatih/color"
-=======
->>>>>>> f047585c7987357528933120d76c179fc5e3a028
 )
 
 func main() {
 	var firstNum, secondNum int
-<<<<<<< HEAD
 	var operators string
 
 	for {
@@ -27,7 +22,7 @@ func main() {
 			break
 		}
 
-		fmt.Println("Enter fist number & second number:")
+		fmt.Println("Enter first number & second number:")
 		_, err = fmt.Scan(&firstNum, &secondNum)
 		if err != nil {
 			color.Red("error:", err)
@@ -35,49 +30,21 @@ func main() {
 		}
 
 		switch operators {
-=======
-	var operation string
-	for {
-
-		fmt.Println("To exit, enter 'q'")
-
-		if operation == "q" {
-			break
-		}
-
-		fmt.Print("Enter two values: ")
-		fmt.Scan(&firstNum, &secondNum)
-
-		fmt.Println("Select operation: '+' '-' '/' '*' 'q'")
-		fmt.Scan(&operation)
-
-		switch operation {
->>>>>>> f047585c7987357528933120d76c179fc5e3a028
 		case "+":
 			fmt.Printf("%d + %d = %d\n", firstNum, secondNum, firstNum+secondNum)
 		case "-":
 			fmt.Printf("%d - %d = %d\n", firstNum, secondNum, firstNum-secondNum)
 		case "/":
 			if secondNum != 0 {
-<<<<<<< HEAD
 				result := float64(firstNum) / float64(secondNum)
 				fmt.Printf("%d / %d = %f\n", firstNum, secondNum, result)
 			} else {
-				color.Red("error")
-=======
-				fmt.Printf("%d / %d = %d\n", firstNum, secondNum, firstNum/secondNum)
-			} else {
-				fmt.Println("error")
->>>>>>> f047585c7987357528933120d76c179fc5e3a028
+				color.Red("error: division by zero")
 			}
 		case "*":
 			fmt.Printf("%d * %d = %d\n", firstNum, secondNum, firstNum*secondNum)
 		default:
-<<<<<<< HEAD
 			color.Red("unknown operator")
-=======
-			fmt.Println("unknown operation:", operation)
->>>>>>> f047585c7987357528933120d76c179fc5e3a028
 		}
 	}
 }
